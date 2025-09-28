49a87ce25de20dbf613f6d05151305621141040f
Juan Antonio Cuello <jacuela@gmail.com>
2025-09-28 20:42:11 +0200

    form1 y form2 incompletos

---
+++ tema2_php/2_formularios/form01/recibe_datos.php
@@ -0,0 +1,30 @@
+<?php
+
+//---------- depuracion --------
+print "<pre>";
+print "Matriz \$_GET" . "<br>";
+print_r($_GET);
+print "</pre>\n";
+
+
+print "<pre>";
+print "Matriz \$_POST" . "<br>";
+print_r($_POST);
+print "</pre>\n";
+
+
+print "<pre>";
+print "Matriz \$_REQUEST" . "<br>";
+print_r($_REQUEST);
+print "</pre>\n";
+
+print "<pre>";
+print "Matriz \$_SERVER" . "<br>";
+print_r($_SERVER);
+print "</pre>\n";
+
+
+
+print "<hr>";
+
+//---------- fin depuracion --------
