59cddeb1797efa196ccd26e7cb41416acc37d878
Juan Antonio Cuello <jacuela@gmail.com>
2025-09-30 20:12:51 +0200

    form2 a mitad

--- tema2_php/2_formularios/form02/index.php
+++ tema2_php/2_formularios/form02/index.php
@@ -1,7 +1,14 @@
 <?php
 
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") { 
     
+    print "<pre>";
+    print_r($_POST);
+    print "</pre>\n";
+
     //Compruebo que existe el campo nombre y que no es vacio
     if (isset($_POST["nombre"]) && $_POST["nombre"]!=""){
         $nombre = trim(htmlspecialchars(strip_tags($_POST["nombre"])));
@@ -62,6 +69,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <label for="edad">Edad:</label>
             <input type="number" id="edad" name="edad">
           </p>
+          <p>
+          <input type="radio" id="sexo_masculino" name="sexo" value="M">
+          <label for="sexo_masculino">MASCULINO</label>
+          <input type="radio" id="sexo_femenino" name="sexo" value="F">
+          <label for="sexo_femenino">FEMENINO</label>
+          <input type="radio" id="sexo_otros" name="sexo" value="O">
+          <label for="sexo_otros">OTROS</label>
+          </p>
+
+          <p>          
+           <input type="checkbox" name="aficiones[]" value="musica"> Musica<br />
+           <input type="checkbox" name="aficiones[]" value="cine"> Cine<br />
+           <input type="checkbox" name="aficiones[]" value="lectura"> Lectura<br />
+          </p>
+
+
+
+
+
+
+
+
         </fieldset>
 
           <p>
